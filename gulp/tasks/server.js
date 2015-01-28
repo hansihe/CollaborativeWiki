e1242ca77dae8b52b5e71e1e73f46007cfc67d56
Hansihe <hansihe@hansihe.com>
2015-01-29 00:15:36 +0100

    update gulp config

--- gulp/tasks/server.js
+++ gulp/tasks/server.js
@@ -1,8 +1,8 @@
 var gulp = require('gulp');
 var config = require('../config').server;
-var makeServer = require('../../src/server/main').makeServer;
 
 gulp.task('server', ['browserify', 'transforms'], function() {
+    var makeServer = require('../../build/js/server/main').makeServer;
     var server = makeServer({});
 
     server.listen(config.port, function() {
--- gulp/tasks/transforms.js
+++ gulp/tasks/transforms.js
@@ -1,8 +1,17 @@
 var gulp = require('gulp');
 var jsx = require('node-jsx');
+var sourceMaps = require('gulp-sourcemaps');
+var to5 = require('gulp-6to5');
+var react = require('gulp-react');
 
 gulp.task('transforms', function() {
-    jsx.install({
-        extension: '.js'
-    });
+    //jsx.install({
+    //    extension: '.js'
+    //});
+    return gulp.src('src/**/*.js')
+        .pipe(sourceMaps.init())
+        .pipe(to5())
+        .pipe(react())
+        .pipe(sourceMaps.write())
+        .pipe(gulp.dest("build/js"));
 });
