32e248625be896b0ce58f9575df36b706d2168b0
Hansihe <hansihe@hansihe.com>
2015-01-29 20:53:03 +0100

    fix static paths

--- src/server/main.js
+++ src/server/main.js
@@ -7,15 +7,6 @@ var ConnectionState = require('./ConnectionState');
 var path = require('path');
 var _ = require('../shared/underscore');
 
-/* var ot = require('ot');
-var o1 = ot.TextOperation.fromJSON(["1"]);
-var o2 = ot.TextOperation.fromJSON([1, "5"]);
-var o3 = ot.TextOperation.fromJSON([1, "8"]);
-
-var no1 = ot.TextOperation.fromJSON([])
-
-console.log(ot.TextOperation.transform(o2, o3)); */
-
 exports.makeServer = function(config) {
     var app = express();
     var httpServer = http.Server(app);
@@ -23,7 +14,7 @@ exports.makeServer = function(config) {
     app.use('/static', express.static('./build/www'));
 
     app.get('*', function(req, res, next) {
-        res.sendFile(path.resolve(__dirname + '../../../build/www/index.html'));
+        res.sendFile(path.resolve(__dirname + '../../../www/index.html'));
     });
 
     var sock = shoe(function(s) {
